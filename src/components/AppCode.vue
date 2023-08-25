6b7475d91ae8141af37229f1e05ba261074a4151
Akifcan Kara <akif@p-ux.net>
2023-08-26 01:42:28 +0300

    updated error component

--- src/components/AppCode.vue
+++ src/components/AppCode.vue
@@ -9,7 +9,7 @@ const codeRef = ref()
 defineProps<Props>()
 const copyButtonText = ref('copy')
 const handleCopy = () => {
-  navigator.clipboard.writeText(codeRef.value.textContent).then(
+  navigator.clipboard.writeText(codeRef.value.textContent.replace(/(\s\s\s*)/g, ' ')).then(
     () => {
       copyButtonText.value = 'Copied'
     },
@@ -28,9 +28,19 @@ const handleCopy = () => {
     </div>
     <div ref="codeRef" class="code--view">
       <div v-for="(x, index) in code" :key="index">
-        <span v-if="x.startsWith('--')">&nbsp; &nbsp;</span>
+        <span v-if="x.startsWith('-')">
+          <span v-for="v in x.split('-').length" :key="v">&nbsp;</span>
+        </span>
+        <!-- <span v-if="x.startsWith('--')">&nbsp; &nbsp;</span>
         <span v-if="x.startsWith('----')">&nbsp; &nbsp; &nbsp; &nbsp;</span>
-        {{ x.replace('--', '').replace('--', '') }}
+        <span v-if="x.startsWith('-----')">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</span> -->
+        {{
+          x
+            .split('')
+            .filter((y) => y !== '-')
+            .join('')
+            .replace('//', '-')
+        }}
       </div>
     </div>
   </div>
--- src/components/docs/ErrorComponent.vue
+++ src/components/docs/ErrorComponent.vue
@@ -4,5 +4,20 @@ import AppCode from '../AppCode.vue'
 
 <template>
   <p>You can show a custom component when an error occured or you can turn off this view.</p>
-  <AppCode title="ErrorView.tsx" :code="['const ErrorView = () => {', '--return <Text>error</Text>;', '};']" />
+  <p>Use <b>setError</b> from <b>useFetchWrapper</b> when you need to close error view back.</p>
+  <AppCode
+    title="ErrorView.tsx"
+    :code="[
+      `import {FetchProvider, useFetchWrapper} from 'fetch//-use'`,
+      'const ErrorView = () => {',
+      `--const {setError} = useFetchWrapper()`,
+      `--return (`,
+      `----<SafeAreaView>`,
+      '------<Text>error view</Text>',
+      `------<Button onPress={() => { setError(false) }} title='Hide' />`,
+      '----</SafeAreaView>',
+      `--)`,
+      '}'
+    ]"
+  />
 </template>
