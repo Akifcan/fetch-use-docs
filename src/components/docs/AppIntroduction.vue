4ecd9510b5803b77d4c3b5b2e9ae70f2d759835c
Akifcan Kara <akif@p-ux.net>
2023-12-02 00:01:11 +0300

    using applications section added

--- src/components/docs/AppIntroduction.vue
+++ src/components/docs/AppIntroduction.vue
@@ -8,5 +8,38 @@
     <li>Timeout/Abourt</li>
     <li>Zero dependencies</li>
   </ul>
-  <p>Copyright: This template from: <a target="_blank" href="https://github.com/charlyllo/doctemplate">charlyllo</a></p>
+  <p>
+    Copyright: This template from:
+    <a target="_blank" rel="external nofollow" href="https://github.com/charlyllo/doctemplate">charlyllo</a>
+  </p>
+  <hr />
+  <h3>Which applications using <b>fetch-use</b>?</h3>
+  <div class="row">
+    <a class="column" target="_blank" rel="external nofollow" href="https://cypien.ai/en">
+      <img src="/cypien-logo.png" class="logo contain" width="150" height="50" alt="Cypien Logo" />
+      <p>
+        <b>Cypien</b> is a generative ai tool. You can create unique blog posts, images and more! You can find the more
+        details via this link.
+      </p>
+    </a>
+  </div>
 </template>
+
+<style scoped>
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+}
+.logo.contain {
+  object-fit: contain;
+}
+.column {
+  display: flex;
+  flex-direction: column;
+}
+
+a {
+  color: #333;
+}
+</style>
